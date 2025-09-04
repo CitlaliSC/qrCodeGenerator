ce76373c218dcf52d76789d9ba5b7d598ed72e4c
CitlaliSC <citlaserratos22@gmail.com>
2025-09-03 23:28:12 -0600

    feat: extension now uses url to create qr codes and download them

--- src/js/generate-qr.js
+++ src/js/generate-qr.js
@@ -1,9 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  generateQR();
+    getCurrentTabUrl().then((url) => {
+        generateQR(url);
+    }).catch((err) => {
+        console.error("Error obteniendo la URL:", err);
+    });
 });
 
-function generateQR() {
-  const qrImage = document.getElementById("qrImage");
-  let link = "Example";
-  qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${link}`;
+function getCurrentTabUrl() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            if (tabs && tabs.length > 0) {
+                resolve(tabs[0].url);
+            } else {
+                reject("No se encontró ninguna pestaña activa");
+            }
+        });
+    });
+}
+
+function generateQR(link) {
+    const qrImage = document.getElementById("qrImage");
+    const qrDownload = document.getElementById("qrDownload");
+
+    const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(link)}`;
+    qrImage.src = qrUrl;
+
+    fetch(qrUrl)
+        .then(res => res.blob())
+        .then(blob => {
+        const blobUrl = URL.createObjectURL(blob);
+        qrDownload.href = blobUrl;
+        qrDownload.download = "qr_code.png"; // fuerza el nombre
+        });
 }
